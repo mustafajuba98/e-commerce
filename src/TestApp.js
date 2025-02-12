4ad630f3feb51ee9fc9c2589b62c57865795532c
Abdelrhman <you@example.com>
2025-02-12 17:27:23 +0200

    search

--- src/TestApp.js
+++ src/TestApp.js
@@ -1,3 +1,40 @@
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import Navbar from "./components/navbar";
+// import Home from "./pages/Home";
+// import Products from "./pages/Products";
+// import Category from "./pages/Category";
+// import Offers from "./pages/Offers";
+// import NotFound from "./pages/notfound";
+// import Footer from './components/footer'
+// import ProductDetails from "./components/ProductDetails";
+// import addedtocart from "./pages/addedtocart";
+// import LoginForm from "./pages/Login";
+// import RegisterForm from './pages/register'
+// function TestApp() {
+  
+  
+//   return (
+//     <Router>
+//       <Navbar />
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//         <Route exact path="/products" component={Products} />
+//         <Route exact path="/product/:id" component={ProductDetails} />
+//         <Route exact path="/category/:categoryName" component={Category} />
+//         <Route exact path="/Login" component={LoginForm} />
+//         <Route exact path="/register" component={RegisterForm} />
+		
+//         <Route exact path="/offers" component={Offers} />
+//         <Route exact path="/addedtocart" component={addedtocart} />
+//         <Route component={NotFound} />
+//       </Switch>
+//       <Footer/>
+//     </Router>
+//   );
+// }
+
+// export default TestApp;
+
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import Home from "./pages/Home";
@@ -9,26 +46,30 @@ import Footer from './components/footer'
 import ProductDetails from "./components/ProductDetails";
 import addedtocart from "./pages/addedtocart";
 import LoginForm from "./pages/Login";
-import RegisterForm from './pages/register'
+import RegisterForm from './pages/register';
+import { SearchProvider } from "./reducers/searchContext"; // Import SearchProvider
+
 function TestApp() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/products" component={Products} />
-        <Route exact path="/product/:id" component={ProductDetails} />
-        <Route exact path="/category/:categoryName" component={Category} />
-        <Route exact path="/Login" component={LoginForm} />
-        <Route exact path="/register" component={RegisterForm} />
-		
-        <Route exact path="/offers" component={Offers} />
-        <Route exact path="/addedtocart" component={addedtocart} />
-        <Route component={NotFound} />
-      </Switch>
-      <Footer/>
-    </Router>
+    <SearchProvider> {/* Wrap your app with SearchProvider */}
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/products" component={Products} />
+          <Route exact path="/product/:id" component={ProductDetails} />
+          <Route exact path="/category/:categoryName" component={Category} />
+          <Route exact path="/Login" component={LoginForm} />
+          <Route exact path="/register" component={RegisterForm} />
+          <Route exact path="/offers" component={Offers} />
+          <Route exact path="/addedtocart" component={addedtocart} />
+          <Route component={NotFound} />
+        </Switch>
+        <Footer />
+      </Router>
+    </SearchProvider>
   );
 }
 
 export default TestApp;
+
